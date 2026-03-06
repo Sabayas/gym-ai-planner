7199fcad891302e1ff2d88537989000ba5fa8fb8
Sabayas <sabayasmin345@gmail.com>
2026-03-07 00:22:40 +0530

    Developing a Gym Ai Planner

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,3 @@
+export default function Auth() {
+    return <div>Welcome to the Auth Page</div>;
+}
---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,3 @@
+export default function Onboarding() {
+    return <div>Welcome to the Onboarding Page</div>;
+}
